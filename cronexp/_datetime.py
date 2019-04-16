c4281e606d4de630d39f9bdab13f2bed243e008f
085astatine <astatine085@gmail.com>
2019-04-17 02:18:44 +0900

    create Dateexp

--- cronexp/_datetime.py
+++ cronexp/_datetime.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import calendar
 from typing import NamedTuple, Optional
-from ._field import Field
+from ._field import Field, month_word_set, weekday_word_set
 
 
 class TimeexpNext(NamedTuple):
     minute: int
     hour: int
     move_up: bool
+
+
+class DateexpNext(NamedTuple):
+    day: int
+    month: int
+    year: int
 
 
 class Timeexp:
@@ -39,3 +46,75 @@ class Timeexp:
     def is_selected(self, minute: int, hour: int) -> bool:
         return (self._minute.is_selected(minute)
                 and self._hour.is_selected(hour))
+
+
+class Dateexp:
+    def __init__(
+            self,
+            day: str,
+            month: str,
+            weekday: str) -> None:
+        self._day = Field(day, 1, 31)
+        self._month = Field(month, 1, 12, word_set=month_word_set())
+        # 0: sunday, ..., 6: saturday
+        self._weekday = Field(weekday, 0, 6, word_set=weekday_word_set())
+
+    def next(self, day: int, month: int, year: int) -> DateexpNext:
+        def next_day(
+                day_: Optional[int],
+                month_: int,
+                year_: int) -> Optional[int]:
+            for day__ in range(1, calendar.monthrange(year_, month_)[1] + 1):
+                if day_ is not None and day__ <= day_:
+                    continue
+                if self.is_selected(day__, month_, year_):
+                    return day__
+            else:
+                return None
+
+        def impl(
+                day_: Optional[int],
+                month_: int,
+                year_: int) -> DateexpNext:
+            if not self._month.is_selected(month_):
+                next_month = self._month.next(month_)
+                if next_month.move_up:
+                    return impl(None, next_month.value, year_ + 1)
+                else:
+                    next_day_ = next_day(None, next_month.value, year_)
+                    if next_day_ is not None:
+                        return DateexpNext(
+                                day=next_day_,
+                                month=next_month.value,
+                                year=year_)
+                    else:
+                        return impl(None, next_month.value + 1, year_)
+            else:
+                day_ = next_day(day_, month_, year_)
+                if day_ is not None:
+                    return DateexpNext(
+                            day=day_,
+                            month=month_,
+                            year=year_)
+                else:
+                    return impl(None, month_ + 1, year_)
+
+        return impl(day, month, year)
+
+    def is_selected(self, day: int, month: int, year: int) -> bool:
+        if not self._month.is_selected(month):
+            return False
+        try:
+            # 0: monday, ...., 6: sunday -> 0: sunday, ..., 6: saturday
+            weekday = (calendar.weekday(year, month, day) + 1) % 7
+        except ValueError:
+            return False
+        if self._day.is_any and self._weekday.is_any:
+            return True
+        elif self._day.is_any and not self._weekday.is_any:
+            return self._weekday.is_selected(weekday)
+        elif not self._day.is_any and self._weekday.is_any:
+            return self._day.is_selected(day)
+        else:
+            return (self._day.is_selected(day)
+                    or self._weekday.is_selected(weekday))
--- test/test_datetime.py
+++ test/test_datetime.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import calendar
+import datetime
 import math
 import unittest
-from cronexp._datetime import Timeexp
+from cronexp._datetime import Dateexp, Timeexp
 
 
 class TimeexpTest(unittest.TestCase):
@@ -74,3 +76,139 @@ class TimeexpTest(unittest.TestCase):
                 self.assertTrue(timeexp.is_selected(
                         minute=result.minute,
                         hour=result.hour))
+
+
+class DateexpTest(unittest.TestCase):
+    def test_every_day(self):
+        dateexp = Dateexp(day='*', month='*', weekday='*')
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            next_date = date + datetime.timedelta(days=1)
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                result = dateexp.next(
+                        year=date.year,
+                        month=date.month,
+                        day=date.day)
+                self.assertEqual(result.day, next_date.day)
+                self.assertEqual(result.month, next_date.month)
+                self.assertEqual(result.year, next_date.year)
+
+    def test_every_month(self):
+        dateexp = Dateexp(day='10', month='*', weekday='*')
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            if date.day < 10:
+                next_month = date.month
+                next_year = date.year
+            elif date.month < 12:
+                next_month = date.month + 1
+                next_year = date.year
+            else:
+                next_month = 1
+                next_year = date.year + 1
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                result = dateexp.next(
+                        year=date.year,
+                        month=date.month,
+                        day=date.day)
+                self.assertEqual(result.day, 10)
+                self.assertEqual(result.month, next_month)
+                self.assertEqual(result.year, next_year)
+
+    def test_yearly(self):
+        dateexp = Dateexp(day='2', month='4', weekday='*')
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            next_year = date.year
+            if date.month > 4 or (date.month == 4 and date.day >= 2):
+                next_year += 1
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                result = dateexp.next(
+                        year=date.year,
+                        month=date.month,
+                        day=date.day)
+                self.assertEqual(result.day, 2)
+                self.assertEqual(result.month, 4)
+                self.assertEqual(result.year, next_year)
+
+    def test_every_weekday(self):
+        weekday_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        for weekday_index, weekday in enumerate(weekday_list):
+            dateexp = Dateexp(day='*', month='*', weekday=weekday)
+            init_date = datetime.date(year=2019, month=1, day=1)
+            for total_days in range(0, 365 + 1):
+                date = init_date + datetime.timedelta(days=total_days)
+                delta_day = 7 - (date.weekday() - weekday_index) % 7
+                next_date = date + datetime.timedelta(days=delta_day)
+                with self.subTest(
+                        weekday=weekday,
+                        year=date.year,
+                        month=date.month,
+                        day=date.day):
+                    result = dateexp.next(
+                            year=date.year,
+                            month=date.month,
+                            day=date.day)
+                    self.assertEqual(next_date.weekday(), weekday_index)
+                    self.assertEqual(result.day, next_date.day)
+                    self.assertEqual(result.month, next_date.month)
+                    self.assertEqual(result.year, next_date.year)
+
+    def test_next(self):
+        dateexp = Dateexp(day='*/10', month='*/3', weekday='*')
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            if date.month % 3 != 1:
+                next_day = 1
+                next_month = math.ceil(date.month / 3) * 3 + 1
+                next_year = date.year
+                if next_month > 12:
+                    next_month %= 12
+                    next_year += 1
+            else:
+                next_day = math.ceil(date.day / 10) * 10 + 1
+                next_month = date.month
+                next_year = date.year
+                if next_day > calendar.monthrange(date.year, date.month)[1]:
+                    next_day = 1
+                    next_month += 3
+                    if next_month > 12:
+                        next_month %= 12
+                        next_year += 1
+            next_date = datetime.date(
+                    year=next_year,
+                    month=next_month,
+                    day=next_day)
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                result = dateexp.next(
+                        year=date.year,
+                        month=date.month,
+                        day=date.day)
+                self.assertEqual(result.day, next_date.day)
+                self.assertEqual(result.month, next_date.month)
+                self.assertEqual(result.year, next_date.year)
+
+    def test_day_or_weekday(self):
+        dateexp = Dateexp(day='*/5', month='*', weekday='Fri')
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            last_day = calendar.monthrange(date.year, date.month)[1]
+            delta_day = min(
+                    7 - (date.weekday() - 4) % 7,
+                    5 - (date.day - 1) % 5
+                    if math.ceil(date.day / 5) * 5 + 1 <= last_day
+                    else 1 + last_day - date.day)
+            next_date = date + datetime.timedelta(days=delta_day)
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                result = dateexp.next(
+                        year=date.year,
+                        month=date.month,
+                        day=date.day)
+                self.assertEqual(result.day, next_date.day)
+                self.assertEqual(result.month, next_date.month)
+                self.assertEqual(result.year, next_date.year)
