d75a6051e1cb3d8c7318195674764fdeb2b36b0d
085astatine <astatine085@gmail.com>
2019-04-25 13:11:33 +0900

    create DayOfMonthField class

--- cronexp/_day_field.py
+++ cronexp/_day_field.py
@@ -1,7 +1,77 @@
 # -*- coding: utf-8 -*-
 
 import calendar
+import re
 from typing import List, Optional
+from ._field import (
+        FieldBase, FieldParseError, FieldElementParseError, parse_field)
+
+
+class DayOfMonthField(FieldBase):
+    def __init__(self, field: str, non_standard: bool) -> None:
+        # initialize base
+        min_ = 1
+        max_ = 31
+        result = parse_field(field, min_, max_)
+        super().__init__(result)
+        # additional variables
+        self._non_standard = non_standard
+        self._is_blank = False
+        self._l = False
+        self._w: List[int] = []
+        # parse not standard
+        mismatched = result.mismatched
+        error = result.error
+        if self._non_standard:
+            mismatched = []
+            for element in result.mismatched:
+                w_match = re.match('^(?P<target>[0-9]+)W$', element)
+                if element == '?':
+                    self._is_blank = True
+                    if field != '?':
+                        error.append(FieldElementParseError(
+                                element,
+                                'there is a charactor other than "?"'))
+                elif element == 'L':
+                    self._l = True
+                elif w_match:
+                    w_target = int(w_match.group('target'))
+                    if min_ <= w_target <= max_:
+                        self._w.append(w_target)
+                    else:
+                        error.append(FieldElementParseError(
+                                element,
+                                '{0} is out of range({1}...{2})'
+                                .format(w_target, min_, max_)))
+                else:
+                    mismatched.append(element)
+        # error check
+        if mismatched or error:
+            raise FieldParseError(
+                    field=field,
+                    mismatched=mismatched,
+                    parse_error=error)
+
+    @property
+    def is_blank(self) -> bool:
+        return self._is_blank
+
+    def next(self, year: int, month: int, day: int) -> Optional[int]:
+        lastday = calendar.monthrange(year, month)[1]
+        if self._non_standard:
+            if self.is_blank:
+                return None
+            target: List[Optional[int]] = []
+            target.append(self.next_value(day))
+            if self._l:
+                target.append(day_of_month_l(year, month, day))
+            target.extend(day_of_month_w(w, year, month, day) for w in self._w)
+            return min(
+                    (x for x in target if x is not None and x <= lastday),
+                    default=None)
+        else:
+            result = self.next_value(day)
+            return result if result is not None and result <= lastday else None
 
 
 def day_of_month_l(year: int, month: int, day: int) -> Optional[int]:
--- test/test_day_field.py
+++ test/test_day_field.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import calendar
+import datetime
+import math
 import unittest
-from cronexp._day_field import day_of_month_l, day_of_month_w
+from cronexp._day_field import DayOfMonthField, day_of_month_l, day_of_month_w
+from cronexp._field import FieldParseError
 
 
 class DayOfMonthLTest(unittest.TestCase):
@@ -70,3 +74,114 @@ class DayOfMonthWTest(unittest.TestCase):
                 self.assertEqual(
                         day_of_month_w(target, year, month, day),
                         result if result > day else None)
+
+
+class DayOfMonthFieldTest(unittest.TestCase):
+    def test_normal(self):
+        field = DayOfMonthField('*/5', non_standard=False)
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            lastday = calendar.monthrange(date.year, date.month)[1]
+            next_day = math.ceil(date.day / 5) * 5 + 1
+            if next_day > lastday:
+                next_day = None
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                self.assertEqual(
+                        field.next(date.year, date.month, date.day),
+                        next_day)
+
+    def test_blank(self):
+        field = DayOfMonthField('?', non_standard=True)
+        self.assertTrue(field.is_blank)
+        date = datetime.date(year=2019, month=1, day=1)
+        self.assertEqual(field.next(date.year, date.month, date.day), None)
+
+    def test_l(self):
+        field = DayOfMonthField('15,L', non_standard=True)
+        init_date = datetime.date(year=2019, month=1, day=1)
+        for total_days in range(0, 365 + 1):
+            date = init_date + datetime.timedelta(days=total_days)
+            lastday = calendar.monthrange(date.year, date.month)[1]
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                self.assertEqual(
+                        field.next(date.year, date.month, date.day),
+                        15 if date.day < 15
+                        else lastday if date.day < lastday
+                        else None)
+
+    def test_w(self):
+        field = DayOfMonthField('10W,20W,30W', non_standard=True)
+        init_date = datetime.date(year=2019, month=1, day=1)
+        expected_table = [
+                [10, 21, 30],  # 2019/01
+                [11, 20, 28],  # 2019/02
+                [11, 20, 29],  # 2019/03,
+                [10, 19, 30],  # 2019/04
+                [10, 20, 30],  # 2019/05
+                [10, 20, 28],  # 2019/06
+                [10, 19, 30],  # 2019/07
+                [9, 20, 30],  # 2019/08
+                [10, 20, 30],  # 2019/09
+                [10, 21, 30],  # 2019/10
+                [11, 20, 29],  # 2019/11
+                [10, 20, 30]]  # 2019/12
+        for total_days in range(0, 365):
+            date = init_date + datetime.timedelta(days=total_days)
+            expected = None
+            for x in expected_table[date.month - 1]:
+                if x > date.day:
+                    expected = x
+                    break
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                self.assertEqual(
+                    field.next(date.year, date.month, date.day),
+                    expected)
+
+    def test_next(self):
+        field = DayOfMonthField('*/5,11W,21W,L', non_standard=True)
+        init_date = datetime.date(year=2019, month=1, day=1)
+        expected_table = [
+                [1, 6, 11, 16, 21, 26, 31],  # 2019/01
+                [1, 6, 11, 16, 21, 26, 28],  # 2019/02
+                [1, 6, 11, 16, 21, 26, 31],  # 2019/03
+                [1, 6, 11, 16, 21, 22, 26, 30],  # 2019/04
+                [1, 6, 10, 11, 16, 21, 26, 31],  # 2019/05
+                [1, 6, 11, 16, 21, 26, 30],  # 2019/06
+                [1, 6, 11, 16, 21, 22, 26, 31],  # 2019/07
+                [1, 6, 11, 12, 16, 21, 26, 31],  # 2019/08
+                [1, 6, 11, 16, 20, 21, 26, 30],  # 2019/09
+                [1, 6, 11, 16, 21, 26, 31],  # 2019/10
+                [1, 6, 11, 16, 21, 26, 30],  # 2019/11
+                [1, 6, 11, 16, 20, 21, 26, 31]]  # 2019/12
+        for total_days in range(0, 365):
+            date = init_date + datetime.timedelta(days=total_days)
+            expected = None
+            for x in expected_table[date.month - 1]:
+                if x > date.day:
+                    expected = x
+                    break
+            with self.subTest(year=date.year, month=date.month, day=date.day):
+                self.assertEqual(
+                    field.next(date.year, date.month, date.day),
+                    expected)
+
+    def test_error_not_question_only(self):
+        field_list = [
+                '*,?',
+                '?,*',
+                '1,?',
+                '?,L']
+        for field in field_list:
+            with self.subTest(field=field):
+                with self.assertRaises(FieldParseError):
+                    DayOfMonthField(field, non_standard=True)
+
+    def test_error_invalid_w(self):
+        field_list = [
+                '0W',
+                '32W']
+        for field in field_list:
+            with self.subTest(field=field):
+                with self.assertRaises(FieldParseError):
+                    DayOfMonthField(field, non_standard=True)
