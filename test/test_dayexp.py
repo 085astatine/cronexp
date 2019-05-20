f0c9363076c8c539bb827bdfda9d06cf04fce6f0
085astatine <astatine085@gmail.com>
2019-05-20 22:44:35 +0900

    add test cases for non standard Dayexp

--- test/test_dayexp.py
+++ test/test_dayexp.py
@@ -122,3 +122,65 @@ class DayexpTest(unittest.TestCase):
                 self.assertEqual(dayexp.next(year, month, day), expected)
                 if expected is not None:
                     self.assertTrue(dayexp.is_selected(year, month, expected))
+
+    def test_day_only_non_standard(self):
+        dayexp = Dayexp(
+                '10-22/3,1W,10W,16W,22W,31W,L',
+                '?',
+                selection_mode=DaySelectionMode.EITHER)
+        year = 2019
+        expected_table = [
+                [1, 10, 13, 16, 19, 22, 31],  # 2019/01
+                [1, 10, 11, 13, 15, 16, 19, 22, 28],  # 2019/02
+                [1, 10, 11, 13, 15, 16, 19, 22, 29, 31],  # 2019/03
+                [1, 10, 13, 16, 19, 22, 30],  # 2019/04
+                [1, 10, 13, 16, 19, 22, 31],  # 2019/05
+                [3, 10, 13, 16, 17, 19, 21, 22, 28, 30],  # 2019/06
+                [1, 10, 13, 16, 19, 22, 31],  # 2019/07
+                [1, 9, 10, 13, 16, 19, 22, 30, 31],  # 2019/08
+                [2, 10, 13, 16, 19, 22, 23, 30],  # 2019/09
+                [1, 10, 13, 16, 19, 22, 31],  # 2019/10
+                [1, 10, 11, 13, 15, 16, 19, 22, 29, 30],  # 2019/11
+                [2, 10, 13, 16, 19, 22, 23, 31]]  # 2019/12
+        for month, day in itertools.product(
+                range(1, 13),
+                (None, *range(1, 32))):
+            expected = min(
+                    filter(lambda x: day is None or x > day,
+                           expected_table[month - 1]),
+                    default=None)
+            with self.subTest(year=year, month=month, day=day):
+                self.assertEqual(dayexp.next(year, month, day), expected)
+                if expected is not None:
+                    self.assertTrue(dayexp.is_selected(year, month, expected))
+
+    def test_weekday_only_non_standard(self):
+        dayexp = Dayexp(
+                '?',
+                'Sun,Sat#2,Sat#4,FriL,SatL',
+                selection_mode=DaySelectionMode.EITHER)
+        year = 2019
+        expected_table = [
+                [6, 12, 13, 20, 25, 26, 27],  # 2019/01
+                [3, 9, 10, 17, 22, 23, 24],  # 2019/02
+                [3, 9, 10, 17, 23, 24, 29, 30, 31],  # 2019/03
+                [7, 13, 14, 21, 26, 27, 28],  # 2019/04
+                [5, 11, 12, 19, 25, 26, 31],  # 2019/05
+                [2, 8, 9, 16, 22, 23, 28, 29, 30],  # 2019/06
+                [7, 13, 14, 21, 26, 27, 28],  # 2019/07
+                [4, 10, 11, 18, 24, 25, 30, 31],  # 2019/08
+                [1, 8, 14, 15, 22, 27, 28, 29],  # 2019/09
+                [6, 12, 13, 20, 25, 26, 27],  # 2019/10
+                [3, 9, 10, 17, 23, 24, 29, 30],  # 2019/11
+                [1, 8, 14, 15, 22, 27, 28, 29]]  # 2019/12
+        for month, day in itertools.product(
+                range(1, 13),
+                (None, *range(1, 32))):
+            expected = min(
+                    filter(lambda x: day is None or x > day,
+                           expected_table[month - 1]),
+                    default=None)
+            with self.subTest(year=year, month=month, day=day):
+                self.assertEqual(dayexp.next(year, month, day), expected)
+                if expected is not None:
+                    self.assertTrue(dayexp.is_selected(year, month, expected))
